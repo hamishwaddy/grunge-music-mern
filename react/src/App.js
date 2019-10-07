cac529826f466e7fcb914d128f6c3ee72af2f0ea
Hamish Wadsworth <hamishwaddy@gmail.com>
2019-10-07 16:33:42 +1300

    setup hoc & Layout folder

--- react/src/App.js
+++ react/src/App.js
@@ -2,12 +2,10 @@ import React, { Fragment } from 'react';
 
 import ShowData from './components/ShowData'
 import AddData from './components/AddData'
-import HeroImage from './components/Layout/HeroImage/heroImage.js'
 
 function App() {
   return (
     <Fragment>
-      <HeroImage />
       <AddData />
       <hr />
       <hr />
